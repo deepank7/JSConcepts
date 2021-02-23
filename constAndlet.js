46330dd068b43eb7f0557d35c37bc4e16c649e7a
deepank7 <36792699+deepank7@users.noreply.github.com>
2021-02-23 17:38:18 +0530

    Add files via upload

---
+++ constAndlet.js
@@ -0,0 +1,12 @@
+//console.log(x);
+getName();
+
+let x = 7;
+
+function getName() {
+    console.log("Hello World!");
+}
+
+getName();
+x = 10;
+console.log(x);
---
+++ hoisting.js
@@ -0,0 +1,11 @@
+console.log(x);
+getName();
+
+var x = 7;
+
+var getName = () => {
+    console.log("Hello World!");
+}
+
+getName();
+console.log(x);
